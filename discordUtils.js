5cfe04e50cf6ab01fd2bee7b7f92f246c74aabfc
Swine <punchmychoda@gmail.com>
2023-08-29 23:20:45 -0400

    Added new features and improvements.

--- discordUtils.js
+++ discordUtils.js
@@ -1,5 +1,5 @@
-const { Client, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
-const { discordChannelId, discordBotToken, debugMode } = require('./settings');
+const { Client, MessageEmbed, MessageActionRow, MessageButton   } = require('discord.js');
+const { discordChannelId, discordBotToken, debugMode, ethThreshold, roleIdToPing } = require('./settings');
 const debugLog = require('./debugUtils');
 
 // Initialize Discord Client
@@ -12,11 +12,28 @@ const client = new Client({
 
 const sendDiscordMessage = async (token) => {
     const channel = client.channels.cache.get(discordChannelId);
+    const balance = parseFloat(token.balance);
+    let embedColor = '#0099ff';
+    let pingText = "";
+    let balanceFieldTitle = 'Owner Balance';
+
+    if (balance > ethThreshold) {
+        embedColor = '#00ff00'; // green
+        balanceFieldTitle = `Owner Balance (Over ${ethThreshold} ETH)`;
+    }
+
+    if (balance > ethThreshold && roleIdToPing) {
+        pingText = `<@&${roleIdToPing}> `;
+    }
+
+
     if (channel) {
         const embedTitle = token.symbol ? `New Token: ${token.symbol}` : 'New Token: Unknown';
         const embed = new MessageEmbed()
-            .setTitle(embedTitle)  // Used a green circle emoji to indicate 'green' <:clown2:1007817304291147796>
-            .setColor('#0099ff');
+            .setTitle(embedTitle)  // <:clown2:1007817304291147796>
+            .setColor(embedColor)
+            .setURL(`https://etherscan.io/address/${token.address}`)
+            .setTimestamp();  
 
         // Add fields with inline: true for name and totalSupply to make them appear in the same row
         embed.addFields(
@@ -25,11 +42,12 @@ const sendDiscordMessage = async (token) => {
             { name: 'Contract Address', value: `[${token.address}](https://etherscan.io/address/${token.address})`, inline: false }
         );
 
-        if (token.symbol) embed.addFields({ name: 'Symbol', value: token.symbol, inline: false });
-        if (token.decimals) embed.addFields({ name: 'Decimals', value: token.decimals, inline: false });
+        //if (token.symbol) embed.addFields({ name: 'Symbol', value: token.symbol, inline: false });
+       // if (token.decimals) embed.addFields({ name: 'Decimals', value: token.decimals, inline: false });
         if (token.owner) embed.addFields({ name: 'Owner', value: token.owner, inline: false });
-        if (token.balance !== undefined) embed.addFields({ name: 'Owner Balance', value: `${token.balance} ETH`, inline: false });
-
+        if (token.balance !== undefined) {
+            embed.addFields({ name: balanceFieldTitle, value: `${token.balance} ETH`, inline: false });
+        }
         // Create buttons
         const dexScreenerButton = new MessageButton()
             .setStyle('LINK')
@@ -44,7 +62,7 @@ const sendDiscordMessage = async (token) => {
         // Create an action row
         const row = new MessageActionRow().addComponents(dexScreenerButton, etherscanButton);
 
-        channel.send({ embeds: [embed], components: [row] })
+        channel.send({ pingText, embeds: [embed], components: [row] })
             .then(() => debugLog(`Message sent!`))
             .catch(err => debugLog(`Failed to send message: ${err}`));
     }
--- settings.js
+++ settings.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 module.exports = {
   
   maxRetries: 3,  //Amount of times a block will be checked before failing and exiting the app.
-  debugMode: true,  // Directly setting it here
+  debugMode: true,  //Debug mode true or false. True will show logging in the console.
+  ethThreshold: 4.9, //This will trigger the alert to show in green and also ping the role ID below.
+  roleIdToPing: '1146277454617514035', //send a ping to a role if ethThreshold is met
   
   //--
   //DO NOT modify below this comment. You must set these values in the .env file. 
